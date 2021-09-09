f36d83661c47987c4d6749d711b35d00dda9970d
sinuhem23 <sinuhem23@gmail.com>
2021-09-09 11:14:38 -0400

    added state

---
+++ src/App.js
@@ -0,0 +1,76 @@
+import './App.css';
+import React, { Component } from 'react';
+
+export class App extends Component {
+  state = {
+    students: [
+      {
+        name: 'Cait Yomorta',
+        bio: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. ',
+        scores: [
+          {
+            date: '2018-02-08',
+            score: 77,
+          },
+          {
+            date: '2018-04-22',
+            score: 92,
+          },
+          {
+            date: '2018-09-15',
+            score: 68,
+          },
+        ],
+      },
+      {
+        name: 'Holly Baird',
+        bio: ' Eos id possimus accusantium, earum animi modi hic.',
+        scores: [
+          {
+            date: '2018-12-14',
+            score: 88,
+          },
+          {
+            date: '2019-01-09',
+            score: 79,
+          },
+          {
+            date: '2019-02-23',
+            score: 91,
+          },
+          {
+            date: '2019-03-01',
+            score: 95,
+          },
+        ],
+      },
+      {
+        name: 'Wes Mungia',
+        bio: 'Enim repellendus ut veniam?',
+        scores: [
+          {
+            date: '2018-10-11',
+            score: 62,
+          },
+          {
+            date: '2018-11-24',
+            score: 74,
+          },
+          {
+            date: '2018-12-19',
+            score: 85,
+          },
+        ],
+      },
+    ],
+  };
+  render() {
+    return (
+      <div className='App'>
+        <h1>hello</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
